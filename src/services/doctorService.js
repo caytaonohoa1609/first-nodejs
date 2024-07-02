2e901456da5d7b4b4428ebb88670836e007fb1dc
caytaonohoa1609 <164864433+caytaonohoa1609@users.noreply.github.com>
2024-07-02 11:04:24 +0700

    Tạo Giao Diện Trang Xem Chi Tiết Thông Tin Bác Sĩ Phía Client

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -12,6 +12,7 @@ let getTopDoctorHome = (limitInput) => {
                 },
                 // raw: true
                 include: [
+                   
                     { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
                     { model: db.Allcode, as: 'genderData', attributes: ['valueEn', 'valueVi'] }
                 ],
@@ -76,8 +77,57 @@ let saveDetailInforDoctor = (inputData) => {
     })
 }
 
+let getDetailDoctorById = (inputId) => {
+    return new Promise( async (resolve, reject) => {
+        try{
+            if(!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter!'
+                })
+            }else {
+                let data = await db.User.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: {
+                        exclude: ['password']
+                    },
+                    include: [
+                        { 
+                            model: db.Markdown, 
+                            attributes: ['description', 'contentHTML', 'contentMarkdown']
+                        },
+
+                        { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+
+                    ],
+                    raw: true,
+                    nest: true
+                })
+
+                if(data && data.image) {
+                    data.image = new Buffer(data.image, 'base64').toString('binary');
+                }
+
+                if(!data) {
+                    data = {};
+                }
+
+                resolve({
+                    errCode: 0,
+                    data: data
+                })
+            }
+        }catch(e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
-    saveDetailInforDoctor: saveDetailInforDoctor
+    saveDetailInforDoctor: saveDetailInforDoctor,
+    getDetailDoctorById: getDetailDoctorById
 }
