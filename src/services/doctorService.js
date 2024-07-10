87385de0c1b51ae1094739c29eca8a37615f4258
caytaonohoa1609 <164864433+caytaonohoa1609@users.noreply.github.com>
2024-07-10 16:08:12 +0700

    Load Động Data Chọn Lịch Khám Bệnh Theo Thời Gian Ngày Tạo

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,7 +1,8 @@
 import { where } from "sequelize";
 import db from "../models/index";
 require('dotenv').config();
-import _ from 'lodash';
+import _, { reject }  from 'lodash';
+import { raw } from "body-parser";
 
 const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 
@@ -122,7 +123,7 @@ let getDetailDoctorById = (inputId) => {
                         { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
 
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true
                 })
 
@@ -169,15 +170,9 @@ let bulkCreateSchedule = (data) => {
                     }
                 );  
 
-                if(existing && existing.length > 0) {
-                    existing = existing.map(item => {
-                        item.date = new Date(item.date).getTime();
-                        return item;
-                    })
-                }
 
                 let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-                    return a.timeType === b.timeType && a.date === b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 });
 
                 if(toCreate && toCreate.length > 0) {
@@ -195,10 +190,46 @@ let bulkCreateSchedule = (data) => {
     })
 }
 
+let getScheduleByDate = (doctorId, date) => {
+    return new Promise( async (resolve, reject) => {
+        try {
+            if (!doctorId || !date) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters'
+                })
+            } else {
+                let dataSchedule = await db.Schedule.findAll({
+                    where: {
+                        doctorId: doctorId,
+                        date: date
+                    },
+
+                    include: [
+                        { model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] },
+
+                    ],
+                    raw: false,
+                    nest: true
+                })
+                if (!dataSchedule) dataSchedule= [];
+
+                resolve({
+                    errCode: 0,
+                    data: dataSchedule
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     saveDetailInforDoctor: saveDetailInforDoctor,
     getDetailDoctorById: getDetailDoctorById,
-    bulkCreateSchedule: bulkCreateSchedule
+    bulkCreateSchedule: bulkCreateSchedule,
+    getScheduleByDate: getScheduleByDate
 }
