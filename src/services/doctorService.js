c962affef2f78df6e82618fb82146b14d5289ebb
caytaonohoa1609 <164864433+caytaonohoa1609@users.noreply.github.com>
2024-07-16 16:11:47 +0700

    Design Giao Diện Hiển Thị Thông Tin Khám Bệnh Của Bác Sĩ

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,7 +1,7 @@
 import { where } from "sequelize";
 import db from "../models/index";
 require('dotenv').config();
-import _, { reject }  from 'lodash';
+import _, { includes, reject }  from 'lodash';
 import { raw } from "body-parser";
 
 const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
@@ -160,6 +160,19 @@ let getDetailDoctorById = (inputId) => {
                         },
 
                         { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+
+                        { 
+                            model: db.Doctor_Infor, 
+                            attributes: {
+                                exclude: ['id', 'doctorId']
+                            },
+                            include: [
+                                { model: db.Allcode, as: 'priceTypeData', attributes: ['valueEn', 'valueVi'] },
+                                { model: db.Allcode, as: 'provinceTypeData', attributes: ['valueEn', 'valueVi'] },
+                                { model: db.Allcode, as: 'paymentTypeData', attributes: ['valueEn', 'valueVi'] },
+
+                            ]
+                        },
 
                     ],
                     raw: false,
