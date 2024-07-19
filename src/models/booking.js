b20b9b50d94bfdf6526540b1955f163db4777147
caytaonohoa1609 <164864433+caytaonohoa1609@users.noreply.github.com>
2024-07-19 15:49:54 +0700

    84 Sử Dụng Sequelize findOrCreate Để Lưu Thông Tin Đặt Lịch Khám Bệnh

--- src/models/booking.js
+++ src/models/booking.js
@@ -16,8 +16,8 @@ module.exports = (sequelize, DataTypes) => {
   Booking.init({
     statusId: DataTypes.STRING,
     doctorId: DataTypes.INTEGER,
-    patienId: DataTypes.INTEGER,
-    date: DataTypes.DATE,
+    patientId: DataTypes.INTEGER,
+    date: DataTypes.STRING,
     timeType: DataTypes.STRING,
 
   }, {
