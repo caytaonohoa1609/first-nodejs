4d3e63b1b98b9eedee523da5bcdc2fbc9fca2495
caytaonohoa1609 <164864433+caytaonohoa1609@users.noreply.github.com>
2024-07-29 20:54:50 +0700

    94 Load Động Data Section Xem Chi Tiết Chuyên Khoa

--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -1,5 +1,6 @@
 const { reject } = require("lodash");
 const db = require("../models");
+const e = require("express");
 
 
 let createSpecialty = (data) => {
@@ -50,7 +51,59 @@ let getAllSpecialty = () => {
     })
 }
 
+let getDetailSpecialtyById = (inputId, location) => {
+    return new Promise ( async (resolve, reject) => {
+        try{
+            if(!inputId || !location) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            }else {
+                let data = await db.Specialty.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: ['descriptionHTML', 'descriptionMarkdown']
+                })
+
+                if(data) {
+                    let doctorSpecialty = [];
+                    if(location === 'ALL') {
+                        doctorSpecialty = await db.Doctor_Infor.findAll({
+                            where: {specialtyId: inputId},
+                            attributes: ['doctorId', 'provinceId']  
+                        })
+                    } else {
+                        // find by location
+                        doctorSpecialty = await db.Doctor_Infor.findAll({
+                            where: {
+                                specialtyId: inputId,
+                                provinceId: location
+                            },
+                            attributes: ['doctorId', 'provinceId']  
+                        })
+                    }
+                    
+                    data.doctorSpecialty = doctorSpecialty;
+
+                }else data = {}
+                resolve({
+                    errCode: 0,
+                    errMessage: 'ok',
+                    data
+                })
+                
+                
+            }
+        }catch(e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     createSpecialty: createSpecialty,
-    getAllSpecialty: getAllSpecialty
+    getAllSpecialty: getAllSpecialty,
+    getDetailSpecialtyById: getDetailSpecialtyById
 }
