3fbd5800fea0e662e55895bcded24c9bbbec68dd
caytaonohoa1609 <164864433+caytaonohoa1609@users.noreply.github.com>
2024-07-31 15:52:15 +0700

    96 Tạo Dữ Liệu Các Cơ Sở Y Tế/Phòng Khám Bệnh

--- src/migrations/migraytion-create-clinic.js
+++ src/migrations/migraytion-create-clinic.js
@@ -17,11 +17,14 @@ module.exports = {
       address: {
         type: Sequelize.STRING
       },
-      description: {
+      image: {
+        type: Sequelize.BLOB('long')
+      },
+      descriptionHTML: {
         type: Sequelize.TEXT
       },
-      image: {
-        type: Sequelize.STRING
+      descriptionMarkdown: {
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
